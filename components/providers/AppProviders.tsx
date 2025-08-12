505c737e36c438ba416c4620a6fac2aa0a8da046
anubispiss <swagatabanerjee02@gmail.com>
2025-08-12 16:26:39 +0530

    Updated Workflows Page

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -1,4 +1,4 @@
-"use client"
+/*"use client"
 export function AppProviders({children}: {children: React.ReactNode}) {
   return <ThemeProvider attribute = "class" defaultTheme="system" enableSystem> {children}</ThemeProvider>
 }
@@ -10,4 +10,25 @@ export function ThemeProvider({
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}*/
+
+"use client";
+
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export function AppProviders({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
